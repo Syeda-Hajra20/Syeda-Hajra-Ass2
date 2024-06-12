968ad387aaf569c742f10d9bb9be6fcf548778a8
Syeda-Hajra20 <syedah204@gmail.com>
2024-06-12 01:31:54 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,19 @@
+#calculate the Area of a Triangle using the formula(0.5*base*height).
+height= float(input("enter the height of the triangle "))
+base= float(input("enter the base of the triangle "))
+area=(0.5)*base*height
+print('the area of the triangle is ',area)
+
+#campute the Area of a Square using the formula(side*side).
+side= float(input("enter the side of the square "))
+area=side*side
+print('the area of the square is ',area)
+
+
+#find the Area of a Circle with a redius of 7 units.formula (πxradius*radius).
+import math
+radius= 7
+area= math.pi *(radius*radius)
+print('the area of the circle with radius of 7 units is ',area)
+
+
